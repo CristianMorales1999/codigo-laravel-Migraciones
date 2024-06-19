c860655ce66e28a8b7e54d642bdd86a0ed16f0ce
CristianMorales1999 <cm9225064@gmail.com>
2024-06-19 15:14:50 -0500

    Vistas y Controladores

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,15 @@
+
+<thead class="table table-bordered">
+    <tr>
+        <th scope="col" class="{{ setActivo('home')}}"><a href="/">Home</a></th>
+        <th scope="col" class="{{ setActivo('nosotros')}}"><a href="nosotros">Nosotros</a></th>
+        <th scope="col" class="{{ setActivo('servicios.index')}}"><a href="servicios">Servicios</a></th>
+        <th scope="col" class="{{ setActivo('contacto')}}"><a href="contacto">Contacto</a></th>
+    </tr>
+    <tr>
+        <td colspan="4">
+            <!-- Agregamos la directiva para agregar contenido dinamico-->
+            @yield('content')
+        </td>
+    </tr>
+</thead>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','home')->name('home');
+Route::view('nosotros','nosotros')->name('nosotros');
+Route::get('servicios','App\Http\Controllers\ServiciosController@index')->name('servicios');
+Route::view('contacto','contacto')->name('contacto');
+Route::resource('servicios','App\Http\Controllers\ServiciosController');
